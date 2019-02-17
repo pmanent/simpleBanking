48689f64f15f1f5c660bf9e1d70b33a5d1535b10
Pere Manent <pmanent@atsistemas.com>
2019-02-17 19:58:06 +0100

    Fix return response token in login service

--- src/main/java/com/simple/banking/security/JWTAuthenticationFilter.java
+++ src/main/java/com/simple/banking/security/JWTAuthenticationFilter.java
@@ -63,5 +63,9 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
                 .withExpiresAt(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
                 .sign(HMAC512(SECRET.getBytes()));
         res.addHeader(HEADER_STRING, TOKEN_PREFIX + token);
+        res.addHeader("Content-Type", "application/json");
+        
+        String tokenResponse = "{\"token\" : \""+token+"\"}";
+        res.getOutputStream().write(tokenResponse.getBytes());
     }
 }
